60fb20ee9b8fe5e798c0701faf2334292c73a3b5
Ferxas <ferxas321@hotmail.es>
2024-02-17 13:37:07 -0500

    adding a new card

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Footer from "./components/Footer.jsx";
 import { FaPencil } from "react-icons/fa6";
 import { PiStudentFill } from "react-icons/pi";
+import backgroundImg from "../public/backgroundUnicor3.jpg";
 
 import "./index.css";
 
@@ -19,7 +20,7 @@ function App() {
                 type="button"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-0 sm:ml-64 mb-1 shadow-lg shadow-cyan-500/50"
               >
-                Editar <FaPencil className="ml-3" />
+                <FaPencil className="mr-2" /> Editar
               </button>
               <p className="text-2xl font-bold mt-1 ml-4 text-slate-800">
                 Aún no tienes contenidos visibles en ésta sección
@@ -27,19 +28,31 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="relative w-full sm:m-40 sm:mb-20 rounded text-black shadow-lg shadow-gray-950 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="bg-transparent p-5 sm:p-20 overflow-hidden ">
-            <h1 className="text-3xl text-slate-300 font-bold relative">Gestión</h1>
-            <button
-              type="button"
-              className="absolute bottom-3 right-0 mr-3 mt-3 text-slate-900 bg-slate-300 hover:bg-slate-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-slate-300 dark:hover:bg-slate-400 dark:focus:ring-slate-100 shadow-lg shadow-slate-500/50"
-            >
-              Editar <FaPencil className="ml-3 text-yellow-300" />
-            </button>
-            <PiStudentFill className="mt-5 sm:mt-10 text-4xl" />
-            <p className="mt-5 sm:mt-10 text-slate-100 ">Acádemico estudiante</p>
+
+        {/* NEW GESTION */}
+        <article
+          className="p-10 min-h-116 max-w-3xl w-full bg-purple-600 rounded-xl text-gray-100 xl:col-span-2 transform duration-500 hover:-translate-y-5 cursor-pointer m-10 shadow-2xl relative"
+          style={{
+            backgroundImage: `url(${backgroundImg})`,
+            backgroundSize: "cover",
+          }}
+        >
+          <h1 className="mt-5 text-5xl font-light text-gray-100 leading-snug hover:underline min-h-33">
+            Gestión
+          </h1>
+          <div className="mt-20 border border-slate-300 w-36 p-3 hover:bg-slate-700">
+            <span className="text-4xl"> <PiStudentFill /> </span>
+            <span className="font-bold text-xl">Acádemico estudiante</span>
           </div>
-        </div>
+          <div className="mt-16 flex justify-between items-center">
+            <button className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800 absolute top-5 right-5">
+              <FaPencil className="text-yellow-300 mr-2" /> Editar
+            </button>
+            <button className="p-3 border-2 border-gray-200 rounded-md text-2xl hover:bg-gray-200 hover:border-gray-200 cursor-pointer hover:text-black">
+              <FaPencil className="text-yellow-300 mr-2" /> Editar
+            </button>
+          </div>
+        </article>
       </div>
     </>
   );
